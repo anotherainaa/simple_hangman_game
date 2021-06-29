2020c28928d2d80b73605bd7106c7f343e933592
Ainaa Azman <ainaasakinah@gmail.com>
2021-06-29 13:28:19 +0900

    change to oop and style changes

--- hangman.rb
+++ hangman.rb
@@ -1,82 +1,72 @@
+require 'yaml'
+
 class HangmanGame
+  attr_reader :correct_guesses, :wrong_guesses, :remaining_guesses
+
   ALPHABET = ("A".."Z").to_a
 
   def initialize
-    words = ["EASY", "MEDIUM"]
-    @secret_word = words.sample
+    generate_secret_word
     @correct_guesses = []
     @wrong_guesses = []
     @remaining_guesses = 7
   end
 
-  def play
-    display_welcome_message
-    loop do
-      display_guesses_remaining
-      display_board
-      player_guess_letter
-      @remaining_guesses -= 1
-      break if player_won? || @remaining_guesses == 0
-    end
-    display_board
-    display_result
-    display_goodbye_message
+  def generate_secret_word
+    wordbank = Psych.load_file("./wordbank.yml")
+    @secret_word = wordbank.sample.upcase
   end
 
-  def player_won?
-    @secret_word.chars.all? { |letter| @correct_guesses.include?(letter) }
+  def valid_letter?(letter)
+    ALPHABET.include?(letter)
   end
 
-  def display_result
-    if player_won?
-      puts "You won!"
-    else
-      puts "No more guesses left! You lose!"
-    end
-    puts ""
+  def valid_guess?(letter)
+    remaining_alphabets.include?(letter)
   end
 
-
-  def display_guesses_remaining
-    puts "You have #{@remaining_guesses} guesses left."
-    puts ""
+  def show_secret_word
+    @secret_word
   end
 
-  def display_welcome_message
-    puts "Welcome to Hangman!"
-    puts ""
-  end
-
-  def display_board
-    word = @secret_word.chars.map do |letter|
+  def secret_word
+    @secret_word.chars.map do |letter|
       if @correct_guesses.include?(letter)
+        " #{letter} "
+      elsif letter =~ /[^A-Z]/
         " #{letter} "
       else
         " _ "
       end
     end.join
-    puts word
-    puts
   end
 
-  def display_goodbye_message
-    puts "Thanks for playing!"
-    puts "Goodbye!"
+  def player_won?
+    @secret_word.delete("^A-Z").chars.all? { |letter| @correct_guesses.include?(letter) }
   end
 
-  def player_guess_letter
-    puts "Pick a letter:"
-    puts ALPHABET.join("-")
-    letter = gets.chomp.upcase
+  def no_remaining_guesses?
+    @remaining_guesses == 0
+  end
 
-    if @secret_word.chars.include?(letter)
-      @correct_guesses << letter
-    elsif !@secret_word.chars.include?(letter)
-      @wrong_guesses << letter
-    end
-    puts ""
+  def remaining_alphabets
+    ALPHABET - @correct_guesses - @wrong_guesses
+  end
+
+  def correct_guess?(letter)
+    @secret_word.include?(letter)
+  end
+
+  def add_correct_guess(letter)
+    @correct_guesses << letter
+  end
+
+  def add_wrong_guess(letter)
+    @wrong_guesses << letter
+  end
+
+  def decrement_remaining_guess
+    @remaining_guesses -= 1
   end
 end
 
-game = HangmanGame.new
-game.play
--- hangman_app.rb
+++ hangman_app.rb
@@ -2,8 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
-
-ALPHABET = ("A".."Z").to_a
+require_relative 'hangman'
 
 configure do
   set :port, 5000
@@ -11,34 +10,9 @@ configure do
   set :sessions_secret, 'secret'
 end
 
-def generate_secret_word
-  words = ["easy", "medium", "mastery", "assessment", "advanced", "launch school", "not yet"]
-  words.sample.upcase
-end
-
-def initialize_game
-  session[:correct_guesses] ||= []
-  session[:wrong_guesses] ||= []
-  session[:remaining_guesses] ||= 7
-  session[:secret_word] ||= generate_secret_word
-end
-
-before '/game' do
-  initialize_game
-end
-
-helpers do
-  def display_secret_word(word)
-    word.chars.map do |letter|
-      if session[:correct_guesses].include?(letter)
-        " #{letter} "
-      elsif letter =~ /[^A-Z]/
-        " #{letter} "
-      else
-        " _ "
-      end
-    end.join
-  end
+def reset_game
+  session.delete(:game)
+  session[:game] ||= HangmanGame.new
 end
 
 get '/' do
@@ -47,64 +21,48 @@ get '/' do
 end
 
 get '/game' do
-  @secret_word = session[:secret_word]
-  @remaining_alphabets = ALPHABET - session[:correct_guesses] - session[:wrong_guesses]
-  @remaining_guesses = session[:remaining_guesses]
+  game = session[:game]
+  @secret_word = game.secret_word
+  @remaining_alphabets = game.remaining_alphabets
+  @remaining_guesses = game.remaining_guesses
+  @alphabets = HangmanGame::ALPHABET
 
   erb :game
 end
 
 def error_for_letter_guess(letter)
-  remaining_alphabets = ALPHABET - session[:correct_guesses] - session[:wrong_guesses]
-  if !ALPHABET.include?(letter)
+  game = session[:game]
+  if !game.valid_letter?(letter)
     "Pick a valid letter."
-  elsif !remaining_alphabets.include?(letter)
-    "Pick a letter that hasn't been picked."
+  elsif !game.valid_guess?(letter)
+    "Pick a letter that hasn't been picked yet."
   end
-end
-
-def player_won?
-  session[:secret_word].delete("^A-Z").chars.all? do |letter|
-    session[:correct_guesses].include?(letter)
-  end
-end
-
-def no_remaining_guesses?
-  session[:remaining_guesses] == 0
-end
-
-def check_game_status
-  redirect '/win' if player_won?
-  redirect '/gameover' if no_remaining_guesses?
 end
 
 post '/game' do
+  game = session[:game]
   letter = params[:letter_guess].capitalize
 
   error = error_for_letter_guess(letter)
   if error
     session[:message] = error
     redirect '/game'
-  elsif session[:secret_word].include?(letter)
-    session[:correct_guesses] << letter
-  elsif !session[:secret_word].chars.include?(letter)
-    session[:wrong_guesses] << letter
-    session[:remaining_guesses] -= 1
+  elsif game.correct_guess?(letter)
+    game.add_correct_guess(letter)
+  elsif !game.correct_guess?(letter)
+    game.add_wrong_guess(letter)
+    game.decrement_remaining_guess
   end
 
-  check_game_status
+  redirect '/win' if game.player_won?
+  redirect '/gameover' if game.no_remaining_guesses?
   redirect '/game'
 end
 
-def reset_game
-  session.delete(:secret_word)
-  session.delete(:correct_guesses)
-  session.delete(:wrong_guesses)
-  session.delete(:remaining_guesses)
-end
 
 get '/gameover' do
-  @secret_word = session[:secret_word]
+  game = session[:game]
+  @secret_word = game.show_secret_word
   erb :gameover
 end
 
