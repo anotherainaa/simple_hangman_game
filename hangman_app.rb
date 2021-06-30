fe94825dcc862def5f279646fff27366efc31f15
Ainaa Azman <ainaasakinah@gmail.com>
2021-06-30 11:54:24 +0900

    add favicon and refactor stuff

--- hangman_app.rb
+++ hangman_app.rb
@@ -12,7 +12,6 @@ end
 
 def reset_game
   session.delete(:game)
-  session[:game] ||= HangmanGame.new
 end
 
 get '/' do
@@ -20,49 +19,53 @@ get '/' do
   erb :index
 end
 
+before do
+  session[:game] ||= HangmanGame.new
+  @game = session[:game]
+end
+
 get '/game' do
-  game = session[:game]
-  @secret_word = game.secret_word
-  @remaining_alphabets = game.remaining_alphabets
-  @remaining_guesses = game.remaining_guesses
+  # game = session[:game]
+  @secret_word = @game.secret_word
+  @remaining_alphabets = @game.remaining_alphabets
+  @remaining_guesses = @game.remaining_guesses
   @alphabets = HangmanGame::ALPHABET
 
   erb :game
 end
 
 def error_for_letter_guess(letter)
-  game = session[:game]
-  if !game.valid_letter?(letter)
+  # game = session[:game]
+  if !@game.valid_letter?(letter)
     "Pick a valid letter."
-  elsif !game.valid_guess?(letter)
+  elsif !@game.valid_guess?(letter)
     "Pick a letter that hasn't been picked yet."
   end
 end
 
 post '/game' do
-  game = session[:game]
+  # game = session[:game]
   letter = params[:letter_guess].capitalize
 
   error = error_for_letter_guess(letter)
   if error
     session[:message] = error
     redirect '/game'
-  elsif game.correct_guess?(letter)
-    game.add_correct_guess(letter)
-  elsif !game.correct_guess?(letter)
-    game.add_wrong_guess(letter)
-    game.decrement_remaining_guess
+  elsif @game.correct_guess?(letter)
+    @game.add_correct_guess(letter)
+  elsif !@game.correct_guess?(letter)
+    @game.add_wrong_guess(letter)
+    @game.decrement_remaining_guess
   end
 
-  redirect '/win' if game.player_won?
-  redirect '/gameover' if game.no_remaining_guesses?
+  redirect '/win' if @game.player_won?
+  redirect '/gameover' if @game.no_remaining_guesses?
   redirect '/game'
 end
 
 
 get '/gameover' do
-  game = session[:game]
-  @secret_word = game.show_secret_word
+  @secret_word = @game.show_secret_word
   erb :gameover
 end
 
